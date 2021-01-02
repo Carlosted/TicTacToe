db1e9763105be8d660c6a3e01d970a983f7a4f55
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2021-01-02 14:02:25 -0500

    Added title and icon

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-import pygame, sys
+import pygame
+
 pygame.init()
 
+# Global variables
 dimensions = (600, 600)
+colorWHITE = (255, 255, 255)
 
-
+# Create screen
 screen = pygame.display.set_mode(dimensions)
 
-screen.fill((255, 255, 255))
-pygame.display.update()
+# Title and icon
+pygame.display.set_caption('TicTacToe', )
+pygame.display.set_icon(pygame.image.load('appIcon.png'))
 
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    # Setting background color to white
+    screen.fill(colorWHITE)
+    pygame.display.update()
