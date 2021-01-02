c2f6a0f7aabdaab5524aa7943358d58c3b63e44c
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2021-01-02 13:24:25 -0500

    Testing if pygame works

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('TicTacToe')
+import pygame, sys
+pygame.init()
+
+dimensions = (600, 600)
+
+
+screen = pygame.display.set_mode(dimensions)
+
+screen.fill((255, 255, 255))
+pygame.display.update()
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
